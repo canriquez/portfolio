23b4f834db77c699761103ab63d68400068cb91c
Carlos Anriquez <canriquez@gmail.com>
2020-11-10 21:19:25 -0300

    add proxy settings on nginx.conf to try avoid the time out issues puma <-> nginx

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,9 @@
 # Change these
 server '34.229.155.242', port: 22, roles: [:web, :app, :db], primary: true
 
+set :rbenv_type, :user # :user or :system, depends on your rbenv setup
+set :rbenv_ruby, File.read('.ruby-version').strip
+
 set :repo_url,        'git@github.com:canriquez/portfolio.git'
 set :application,     'portfolio'
 set :user,            'ubuntu'
