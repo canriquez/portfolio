20f5352a446e9e536b2bd9a6324ae67d6f1e879e
Carlos Anriquez <canriquez@gmail.com>
2020-11-11 10:28:28 -0300

    muting on_worker_boot do in puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -22,8 +22,8 @@ pidfile "#{shared_dir}/pids/puma.pid"
 state_path "#{shared_dir}/pids/puma.state"
 activate_control_app
 
-on_worker_boot do
-  require "active_record"
-  ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
-  ActiveRecord::Base.establish_connection(YAML.load_file("#{app_dir}/config/database.yml")[rails_env])
-end
+#on_worker_boot do
+#  require "active_record"
+#  ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
+#  ActiveRecord::Base.establish_connection(YAML.load_file("#{app_dir}/config/database.yml")[rails_env])
+#end
